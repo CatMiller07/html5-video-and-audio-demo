749878992511c262a0fa9c19c37a723b1aef9314
catmiller07 <catmiller07@comcast.net>
2016-01-13 21:25:53 -0500

    Third Commit

--- audioplayer.js
+++ audioplayer.js
@@ -1,7 +1,22 @@
-var m$=document.getElementById;
+if (localStorage.getItem("lastThingPlayed")){
+	
+	document.getElementById("audioguy").setAttribute("src",
+												 localStorage.getItem("lastThingPlayed"));
+}
 
 document.getElementById("load-audio").onclick = function(){
 	document.getElementById("audioguy").setAttribute("src",
 	                                    document.getElementById("source-input").value
 										);
+};
+
+document.getElementById("slowdown").onclick = function() {
+	document.getElementById("audioguy").playbackRate -=0.01;
+};
+
+document.getElementById("speedup").onclick = function() {
+	document.getElementById("audioguy").playbackRate +=0.01;
+};
+document.getElementById("speedreset").onclick = function() {
+	document.getElementById("audioguy").playbackRate = ;
 };
