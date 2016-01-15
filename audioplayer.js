cf1cc03b654589c7387b7ce55db19c11d1053f71
catmiller07 <catmiller07@comcast.net>
2016-01-14 21:52:23 -0500

    commit

--- audioplayer.js
+++ audioplayer.js
@@ -1,3 +1,22 @@
+$(function () {
+    $('#fileupload').fileupload({
+			dataType: 'json',
+			done: function (e, data) {
+				$.each(data.result.files, function (index, file) {
+					$('<p/>').text(file.name).appendTo(document.body);
+				});
+			} ,
+		progressall: function (e, data) {
+        var progress = parseInt(data.loaded / data.total * 100, 10);
+        $('#progress .bar').css(
+            'width',
+            progress + '%'
+        );
+    }
+    });
+});
+
+
 if (localStorage.getItem("lastThingPlayed")){
 	
 	document.getElementById("audioguy").setAttribute("src",
